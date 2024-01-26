8239b89795faddf5af54b07bf32a657c3a81cb40
aqubi <msp827@naver.com>
2024-01-26 16:19:07 +0900

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 50vw;
+  gap: 20px;
+`;
+
+const Box = styled(motion.div)`
+  height: 300px;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Grid>
+        <Box />
+        <Box><Circle /></Box>
+        <Box><Circle /></Box>
+        <Box />
+      </Grid>
+    </Wrapper>
+  );
+}
+
+export default App;
